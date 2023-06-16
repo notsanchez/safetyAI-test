f64203538a68014e6ca6d6a2b52e52f5be5efdb2
Matheus Machado <matheus.machado@beaverbit.com.br>
2023-06-16 17:21:13 -0300

    test

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -7,49 +6,70 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+import os
+import glob
+from flask import Flask, request, make_response, jsonify
+import json
+import codecs
+import ast
 
-
-def main():
+def main(ask):
     load_dotenv()
-    st.set_page_config(page_title="Ask your PDF")
-    st.header("Ask your PDF 💬")
     
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    file_path = "./documents"
+    chunks = []
     
-    # extract the text
-    if pdf is not None:
-      pdf_reader = PdfReader(pdf)
-      text = ""
-      for page in pdf_reader.pages:
-        text += page.extract_text()
-        
-      # split into chunks
-      text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-      )
-      chunks = text_splitter.split_text(text)
-      
-      # create embeddings
-      embeddings = OpenAIEmbeddings()
-      knowledge_base = FAISS.from_texts(chunks, embeddings)
-      
-      # show user input
-      user_question = st.text_input("Ask a question about your PDF:")
-      if user_question:
-        docs = knowledge_base.similarity_search(user_question)
-        
+    arquivos_pdf = []
+
+    for arquivo in glob.glob(os.path.join(file_path, "*.pdf")):
+        arquivos_pdf.append(arquivo)
+
+        for arquivo_pdf in arquivos_pdf:
+            
+            with open(arquivo_pdf, "rb") as file:
+
+                if file is not None:
+                    pdf_reader = PdfReader(file)
+                    text = ""
+                    for page in pdf_reader.pages:
+                        text += page.extract_text()
+
+                    text_splitter = CharacterTextSplitter(
+                        separator="\n",
+                        chunk_size=1000,
+                        chunk_overlap=200,
+                        length_function=len
+                    )
+                    
+                    for chunk in text_splitter.split_text(text):
+                        chunks.append(chunk)
+
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+    while True:
+        user_question = ask
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+
         llm = OpenAI()
         chain = load_qa_chain(llm, chain_type="stuff")
         with get_openai_callback() as cb:
-          response = chain.run(input_documents=docs, question=user_question)
-          print(cb)
-           
-        st.write(response)
-    
+            response = chain.run(input_documents=docs, question=user_question)
+        
+        return str(response)
 
-if __name__ == '__main__':
-    main()
+
+app = Flask(__name__)
+
+@app.route('/safety-ask', methods=['GET'])
+def get():
+    
+    ask = request.args.get('value')
+    
+    response = main(ask)
+
+    return response
+
+app.run()
+        
